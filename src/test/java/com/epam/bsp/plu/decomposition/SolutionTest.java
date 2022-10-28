184fa6ac0426d3e6f919aa6f8017d19f81c32cb3
Viktar Liauchuk <viktar_liauchuk@epam.com>
2022-10-28 14:46:41 +0300

    version 0 initial refactored.

--- src/test/java/com/epam/bsp/plu/decomposition/SolutionTest.java
+++ src/test/java/com/epam/bsp/plu/decomposition/SolutionTest.java
@@ -1,6 +1,7 @@
 package com.epam.bsp.plu.decomposition;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
@@ -55,9 +56,7 @@ public class SolutionTest {
 				{2, 1, 1}
 		};
 		
-		double[][] matrixCopy = getMatrixCopy(matrix);
-		
-		double[][] matrixInv = Solution.getInverseMatrix(matrixCopy);
+		double[][] matrixInv = Solution.getInverseMatrix(matrix);
 		
 		double[][] identityCalc = getMultiplication(matrix, matrixInv);
 		
@@ -72,7 +71,7 @@ public class SolutionTest {
 	}
 	
 	private static void assertMatricesEqual(double[][] expected, double[][] initial) {
-		assertTrue(expected.length == initial.length);
+		assertEquals(expected.length, initial.length);
 		
 		assertTrue(IntStream.range(0, expected.length)
 				.allMatch(i -> expected[i].length == initial[i].length));
@@ -91,20 +90,12 @@ public class SolutionTest {
 				.toArray();
 	}
 
-	private static double[][] getMatrixCopy(double[][] matrix) {
-		return IntStream.range(0, matrix[0].length)
-				.mapToObj(r -> IntStream.range(0, matrix.length)
-						.mapToDouble(c -> matrix[c][r])
+	private static double[][] getMultiplication(double[][] a, double[][] b) {
+		return Arrays.stream(a).map(doubles -> IntStream.range(0, b[0].length)
+						.mapToDouble(j -> IntStream.range(0, b.length)
+								.mapToDouble(k -> doubles[k] * b[k][j]).sum())
 						.toArray())
 				.toArray(double[][]::new);
 	}
 
-	private static double[][] getMultiplication(double[][] a, double[][] b) {
-		return IntStream.range(0, a.length)
-				.mapToObj(i -> IntStream.range(0, b[0].length)
-						.mapToDouble(j -> IntStream.range(0, b.length)
-								.mapToDouble(k -> a[i][k] * b[k][j]).sum())
-						.toArray())
-				.toArray(double[][]::new);
-	}
 }
