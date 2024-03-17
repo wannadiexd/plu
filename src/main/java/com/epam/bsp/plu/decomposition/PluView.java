e6fccfff35d44e8a9c5f26f40a4a35742251a547
arslan_diveyev <arslan_diveyev@student.itpu.uz>
2024-03-17 22:47:35 +0000

    Update src/main/java/com/epam/bsp/plu/decomposition/PluView.java

--- src/main/java/com/epam/bsp/plu/decomposition/PluView.java
+++ src/main/java/com/epam/bsp/plu/decomposition/PluView.java
@@ -1,30 +1,25 @@
 package com.epam.bsp.plu.decomposition;
 
 public class PluView {
-	private double[][] perm;
-	private double[][] lower;
-	private double[][] upper;
+    private double[][] perm;
+    private double[][] lower;
+    private double[][] upper;
 
-	public PluView() {
-	}
+    public PluView(double[][] perm, double[][] lower, double[][] upper) {
+        this.perm = perm;
+        this.lower = lower;
+        this.upper = upper;
+    }
 
-	public PluView(double[][] perm, double[][] lower, double[][] upper) {
-		super();
-		this.perm = perm;
-		this.lower = lower;
-		this.upper = upper;
-	}
+    public double[][] getPerm() {
+        return perm;
+    }
 
-	public double[][] getPerm() {
-		return perm;
-	}
+    public double[][] getLower() {
+        return lower;
+    }
 
-	public double[][] getLower() {
-		return lower;
-	}
-
-	public double[][] getUpper() {
-		return upper;
-	}
-	
+    public double[][] getUpper() {
+        return upper;
+    }
 }
